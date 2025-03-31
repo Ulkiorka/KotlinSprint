9d1aba05f608f6ecebf17a99bfd5e2acf1101392
Alexey Karelskiy <ulkiorka@gmail.com>
2025-03-31 13:26:43 +0200

    KS-1-5 fixing reviewer's notes about constants and magic numbers

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,9 +2,11 @@ package org.example.lesson_1
 
 fun main() {
     val totalSecondsInSpace: Int = 6480
-    val hours: Int = totalSecondsInSpace / 3600
-    val minutes: Int = totalSecondsInSpace / 60 % 60
-    val seconds: Int = totalSecondsInSpace % 60
+    val minutesInHour: Int = 60
+    val secondsInMinute: Int = 60
+    val hours: Int = totalSecondsInSpace / (minutesInHour * secondsInMinute)
+    val minutes: Int = totalSecondsInSpace / secondsInMinute % minutesInHour
+    val seconds: Int = totalSecondsInSpace % secondsInMinute
     val formattedTimeInSpace : String = String.format("%02d:%02d:%02d", hours, minutes, seconds)
     println(formattedTimeInSpace)
 }
