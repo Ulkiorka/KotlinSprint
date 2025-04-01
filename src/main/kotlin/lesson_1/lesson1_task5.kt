f24540c06d127841cf76a4812098900d10c10067
Alexey Karelskiy <ulkiorka@gmail.com>
2025-04-01 14:15:52 +0200

    KS-1-5 Extracting constants from a function

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_1
 
+const val MINUTES_IN_HOUR = 60
+const val SECONDS_IN_MINUTE = 60
+
 fun main() {
     val totalSecondsInSpace: Int = 6480
-    val minutesInHour: Int = 60
-    val secondsInMinute: Int = 60
-    val hours: Int = totalSecondsInSpace / (minutesInHour * secondsInMinute)
-    val minutes: Int = totalSecondsInSpace / secondsInMinute % minutesInHour
-    val seconds: Int = totalSecondsInSpace % secondsInMinute
+    val hours: Int = totalSecondsInSpace / (MINUTES_IN_HOUR * SECONDS_IN_MINUTE)
+    val minutes: Int = totalSecondsInSpace / SECONDS_IN_MINUTE % MINUTES_IN_HOUR
+    val seconds: Int = totalSecondsInSpace % SECONDS_IN_MINUTE
     val formattedTimeInSpace : String = String.format("%02d:%02d:%02d", hours, minutes, seconds)
     println(formattedTimeInSpace)
 }
