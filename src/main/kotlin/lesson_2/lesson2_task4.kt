eb66bc8829c84f335a75ac5caf9495bccf41d522
Alexey Karelskiy <ulkiorka@gmail.com>
2025-04-05 22:50:53 +0200

    KS-2-4 fixing reviewer's notes

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,9 +1,10 @@
 package org.example.lesson_2
 
 fun main(){
+    val amountPercentage = 20
     val crystalWithoutBuff = 7
     val ironWithoutBuff = 11
-    val buffMultiplier = 0.20
+    val buffMultiplier = amountPercentage.toDouble() / 100
     val crystalBonus = (crystalWithoutBuff * buffMultiplier).toInt()
     val ironBonus = (ironWithoutBuff * buffMultiplier).toInt()
 
