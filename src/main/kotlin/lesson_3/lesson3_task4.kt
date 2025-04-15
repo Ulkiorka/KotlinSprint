847e93b38fd57a91e8eb1c4d514c8f4df76fb48f
Alexey Karelskiy <ulkiorka@gmail.com>
2025-04-15 21:55:43 +0200

    KS-3-4 - printing E2-E4

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main(){
+    var from = "E2"
+    var to = "E4"
+    var moveCounter = 1
+
+    println("Ход №$moveCounter: $from-$to")
+
+    moveCounter++
+    from = "D2"
+    to = "D3"
+
+    println("Ход №$moveCounter: $from-$to")
+}
