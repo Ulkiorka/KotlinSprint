d134cf2cfba6ef2c0c7c6fd519c86158ac4ed51b
Alexey Karelskiy <ulkiorka@gmail.com>
2025-04-18 21:52:49 +0200

    KS-3-5 - 1 variable instead of 3

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,11 +3,9 @@ package org.example.lesson_3
 fun main(){
     val serverString = "D2-D4;0"
 
-    val from = serverString.split("-")[0]
-    val to = serverString.split("-",";")[1]
-    val counter = serverString.split(";")[1].toInt()
+    val splittedServerString = serverString.split("-", ";")
 
-    println("Откуда: $from")
-    println("Куда: $to")
-    println("Номер хода: $counter")
+    println("Откуда: ${splittedServerString[0]}")
+    println("Куда: ${splittedServerString[1]}")
+    println("Номер хода: ${splittedServerString[2]}")
 }
