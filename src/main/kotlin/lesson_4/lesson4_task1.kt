1dfb95d057ff2b3821275014d4bd77ba3f674f58
Alexey Karelskiy <ulkiorka@gmail.com>
2025-04-20 21:58:05 +0200

    KS-4-1 - free tables in the restaurant

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -4,6 +4,7 @@ fun main(){
     val totalAmount = 13
     val today = 13
     val tomorrow = 9
+
     println("[Доступность столиков на сегодня: ${today < totalAmount}],")
     print( "[Доступность столиков на завтра: ${tomorrow < totalAmount}].")
 }
