83ebbf9ec6f39190e23b3e2b7dff12cffa43caf4
Alexey Karelskiy <ulkiorka@gmail.com>
2025-04-24 21:51:28 +0200

    KS-4-3 - extracting constants from a function

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,11 +1,12 @@
 package org.example.lesson_4
 
+const val REQUIRED_HUMIDITY = 20
+const val FORBIDDEN_SEASON = "зима"
+
 fun main(){
     val isSunnyWeather : Boolean = true
     val isTentOpen : Boolean = true
-    val humidity : Int = 20
-    val currentSeason : String = "зима"
 
-    val suitableConditions = isSunnyWeather && isTentOpen && humidity == 20 && currentSeason != "зима"
+    val suitableConditions = isSunnyWeather && isTentOpen && REQUIRED_HUMIDITY == 20 && FORBIDDEN_SEASON != "зима"
     println("Благоприятные ли условия сейчас для роста бобовых? $suitableConditions")
 }
