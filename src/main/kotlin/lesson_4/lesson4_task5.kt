0cbf892a8fe0177c68a2589270dc259c0840c2e9
Alexey Karelskiy <ulkiorka@gmail.com>
2025-04-27 00:11:05 +0200

    KS-4-5 - converting magic numbers to constants

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,4 +1,10 @@
 package org.example.lesson_4
+
+const val MIN_CREW = 55
+const val MAX_CREW = 70
+const val NORMAL_AMOUNT_FOOD_BOXES = 50
+
+
 fun main() {
     print("Отсутствуют повреждения (true или false): ")
     val hasNoDamaged = readln().toBoolean()
@@ -9,8 +15,8 @@ fun main() {
     print("Погода благоприятна (true или false): ")
     val isGoodWeather = readln().toBoolean()
 
-    val canSail = (hasNoDamaged && crewCount in 55..70 && foodBoxes > 50) ||
-            (crewCount == 70 && isGoodWeather && foodBoxes >= 50)
+    val canSail = (hasNoDamaged && crewCount in MIN_CREW..MAX_CREW && foodBoxes > NORMAL_AMOUNT_FOOD_BOXES) ||
+            (crewCount == MAX_CREW && isGoodWeather && foodBoxes >= NORMAL_AMOUNT_FOOD_BOXES)
 
     println("Судно может отправиться в рейс: $canSail")
 }
