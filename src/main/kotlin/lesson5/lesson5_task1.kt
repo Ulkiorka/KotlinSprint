e802ce375b9a6dfbae09fda91d3e4fba261f8fe9
Alexey Karelskiy <ulkiorka@gmail.com>
2025-05-06 22:27:27 +0200

    KS-5-1 - code correction according to reviewer's comments

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,10 +1,8 @@
 package org.example.lesson5
 
-import kotlin.random.Random
-
 fun main(){
-    val number1 = Random.nextInt(0, 9)
-    val number2 = Random.nextInt(0, 9)
+    val number1 =  (1..10).random()
+    val number2 =  (1..10).random()
     val correctSum = number1 + number2
 
     println("Докажите, что вы не бот. Решите пример: $number1 + $number2 = ?")
