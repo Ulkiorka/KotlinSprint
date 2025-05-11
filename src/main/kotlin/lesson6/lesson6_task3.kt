bc472982de3d716e6712418972852655805c68ff
Alexey Karelskiy <ulkiorka@gmail.com>
2025-05-11 21:15:06 +0200

    KS-6-3 - new timer (update)

--- src/main/kotlin/lesson6/lesson6_task3.kt
+++ src/main/kotlin/lesson6/lesson6_task3.kt
@@ -1,11 +1,10 @@
 package org.example.lesson6
 
 fun main(){
-    val seconds = readln().toInt()
-    var timer = seconds
-    while (timer > 0){
+    var seconds = readln().toInt()
+    while (seconds > 0){
         Thread.sleep(1000)
-        println("Осталось секунд: ${--timer}")
+        println("Осталось секунд: ${--seconds}")
     }
-    println("Прошло $seconds секунд")
+    println("Время вышло")
 }
