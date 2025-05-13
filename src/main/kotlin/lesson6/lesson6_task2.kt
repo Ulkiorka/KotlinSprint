ef4130d9d9a324a31658a6fc444cdf5b14bef70b
Alexey Karelskiy <ulkiorka@gmail.com>
2025-05-13 22:02:39 +0200

    KS-6-2 - removing cycle

--- src/main/kotlin/lesson6/lesson6_task2.kt
+++ src/main/kotlin/lesson6/lesson6_task2.kt
@@ -1,11 +1,7 @@
 package org.example.lesson6
 
 fun main(){
-    val seconds = readln().toInt()
-    var timer = seconds
-    while (timer > 0){
-        timer--
-        Thread.sleep(1000)
-    }
+    val seconds = readln().toLong()
+        Thread.sleep(seconds * 1000)
     println("Прошло $seconds секунд")
 }
