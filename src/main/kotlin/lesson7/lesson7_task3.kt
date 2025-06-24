fe933116dd827c75a8b4891b9d71220bbacb6b01
Alexey Karelskiy <ulkiorka@gmail.com>
2025-06-24 16:49:10 +0200

    KS-7-3 - progression of even numbers until user's number

---
+++ src/main/kotlin/lesson7/lesson7_task3.kt
@@ -0,0 +1,8 @@
+package org.example.lesson7
+
+fun main() {
+    println("Введите число:")
+    val userNumber = readln().toInt()
+    for (number in 0..userNumber step 2)
+        println(number)
+}
