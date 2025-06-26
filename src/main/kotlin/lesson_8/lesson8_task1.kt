6bdc0bd680ee38f1b5810a975789c57f0ed7a8a4
Alexey Karelskiy <ulkiorka@gmail.com>
2025-06-26 14:58:32 +0200

    KS-8-2: Removing redundant files from previous task that were mistakenly included in previous commit

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++
@@ -1,9 +0,0 @@
-package org.example.lesson_8
-
-fun main(){
-        val viewsPerDay = arrayOf(10000, 5000, 777, 1000, 500)
-        var totalViews = 0
-        for (dayViews in viewsPerDay)
-            totalViews += dayViews
-        println("Просмотров за неделю: $totalViews")
-}
