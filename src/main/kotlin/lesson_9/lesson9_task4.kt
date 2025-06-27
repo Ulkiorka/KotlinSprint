1985d7345b46ff19c179318023bff311f0057b20
Alexey Karelskiy <ulkiorka@gmail.com>
2025-06-27 14:27:42 +0200

    KA-9-4 removing redundant code

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -2,8 +2,7 @@ package org.example.lesson_9
 fun main(){
     println("Введите 5 ингредиентов, разделённых запятой с пробелом")
     val inputText = readln().trim()
-    val listeDeProduites = inputText.split(", ").toMutableList()
+    val listeDeProduites = inputText.split(", ").sorted()
     println("В рецепте есть следующие ингредиенты: ")
-    val sortedList = listeDeProduites.sorted()
-    println(sortedList)
+    println(listeDeProduites)
 }
