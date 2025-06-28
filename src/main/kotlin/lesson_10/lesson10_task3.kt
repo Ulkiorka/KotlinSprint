ccb31b835934ed0159e8b58aebdeb3d21e159cf0
Alexey Karelskiy <ulkiorka@gmail.com>
2025-06-28 14:31:39 +0200

    KS-10-3 fixed several reviewer's notes

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -1,16 +1,17 @@
 package org.example.lesson_10
 
-fun main(){
+fun main() {
     println("Введите длину пароля")
     val length = readln().toInt()
     println(generatePassword(length))
 }
 
-fun generatePassword(len: Int) : String{
-    var password = StringBuilder()
-    var digits = (0..9)
-    val symbols = """!"#$%&'()*+,-./ """.toList()
-    for (i in 1..len){
+fun generatePassword(length: Int): String {
+    val password = StringBuilder()
+    val digits = '0'..'9'
+    val symbols = ' '..'/'
+
+    for (i in 1..length) {
         if (i % 2 != 0)
             password.append(digits.random())
         else
