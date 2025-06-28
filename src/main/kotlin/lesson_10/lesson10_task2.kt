b9a77e4ae838b8c591ef10ab95c4d3c62e7a5651
Alexey Karelskiy <ulkiorka@gmail.com>
2025-06-28 13:53:53 +0200

    KS-10-2 Removing extra spaces before comma

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -7,10 +7,10 @@ fun main() {
     println("Введите пароль")
     val password = readln()
 
-    if (validate(login , password))
+    if (validate(login, password))
         println("Welcome to the club buddy")
     else
         println("Логин или пароль недостаточно длинные")
 }
 
-fun validate(login: String , password: String) = (login.length >= 4 && password.length >= 4)
+fun validate(login: String, password: String) = (login.length >= 4 && password.length >= 4)
