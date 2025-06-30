44e183baac3ffee21ae29354a62fe5511b42da94
Alexey Karelskiy <ulkiorka@gmail.com>
2025-06-30 16:15:13 +0200

    KS-11-1 refactoring the files according to lesson description

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -1,5 +1,13 @@
 package org.example.lesson_11
 
+fun main(){
+    val user1 = User(1,"Пикачу", "pika-pika", "mail@email.pika")
+    val user2 = User(2,"Слоупок", "Yadon", "mail@email.yadon")
+    user1.showAllFields()
+    println()
+    user2.showAllFields()
+}
+
 class User(
     val userId: Int,
     val login: String,
