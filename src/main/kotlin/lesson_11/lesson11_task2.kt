00df937ee3896474919a62d5ce03b60dd1b2767e
Alexey Karelskiy <ulkiorka@gmail.com>
2025-06-30 16:32:40 +0200

    KS-11-2 changing password and adding bio

---
+++ src/main/kotlin/lesson_11/lesson11_task2.kt
@@ -0,0 +1,46 @@
+package org.example.lesson_11
+
+fun main() {
+    val user = User(1 , "Пикачу" , "12345" , "mail@email.pika")
+    user.showAllFields()
+    println()
+    user.changeBio()
+    println()
+    user.showAllFields()
+    user.changeLogin()
+    user.showAllFields()
+}
+
+class User(
+    val userId: Int ,
+    val login: String ,
+    var password: String ,
+    val email: String ,
+    var bio: String = "" ,
+) {
+    fun showAllFields() {
+        println("userId = $userId")
+        println("login = $login")
+        println("password = $password")
+        println("email = $email")
+        println("bio = $bio")
+    }
+
+    fun changeBio() {
+        println("Введите текст описания: ")
+        bio = readln()
+    }
+
+    fun changeLogin(){
+        println("Введите текущий пароль: ")
+        val text = readln()
+        if (password == text){
+            println("Введите новый пароль: ")
+            password = readln()
+        }
+        else{
+            println("Неверный пароль")
+            return
+        }
+    }
+}
