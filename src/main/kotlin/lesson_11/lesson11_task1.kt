4cef5022c421c7532ef9d57c6130092d225ae70a
Alexey Karelskiy <ulkiorka@gmail.com>
2025-07-01 16:43:02 +0200

    refactoring variable and class names to eliminate conflicts between code from different tasks

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_11
 
 fun main() {
-    val user1 = User(1 , "Пикачу" , "pika-pika" , "mail@email.pika")
-    val user2 = User(2 , "Слоупок" , "Yadon" , "mail@email.yadon")
+    val user1 = User1(1 , "Пикачу" , "pika-pika" , "mail@email.pika")
+    val user2 = User1(2 , "Слоупок" , "Yadon" , "mail@email.yadon")
     user1.showAllFields()
     println()
     user2.showAllFields()
 }
 
-class User(
+class User1(
     val userId: Int ,
     val login: String ,
     val password: String ,
