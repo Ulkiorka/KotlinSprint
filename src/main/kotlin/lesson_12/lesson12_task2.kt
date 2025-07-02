958bb397cfd843c469b890ac858ef35193ff8f2e
Alexey Karelskiy <ulkiorka@gmail.com>
2025-07-02 16:21:04 +0200

    KS-12-2 Using automatic code formatting

--- src/main/kotlin/lesson_12/lesson12_task2.kt
+++ src/main/kotlin/lesson_12/lesson12_task2.kt
@@ -1,8 +1,8 @@
 package org.example.lesson_12
 
 fun main() {
-    val today = weather_1(10,-10, true)
-    val yesterday = weather_1(0, 5, false)
+    val today = weather_1(10 , -10 , true)
+    val yesterday = weather_1(0 , 5 , false)
 
     today.printData()
     yesterday.printData()
@@ -12,9 +12,9 @@ fun main() {
 }
 
 class weather_1(
-    _daytimeTemperature : Int ,
-    _nighttimeTemperature : Int ,
-    _precipitation : Boolean ,
+    _daytimeTemperature: Int ,
+    _nighttimeTemperature: Int ,
+    _precipitation: Boolean ,
 ) {
     var daytimeTemperature = _daytimeTemperature
     var nighttimeTemperature = _nighttimeTemperature
