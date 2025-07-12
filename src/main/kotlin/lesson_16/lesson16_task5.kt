233e358945d02a41d5e24e4b2abea2c780acb2f7
Alexey Karelskiy <ulkiorka@gmail.com>
2025-07-12 18:27:23 +0200

    KS-16-5 fixing reviewer notes. Adding variable for to checking is player is still alive or not

--- src/main/kotlin/lesson_16/lesson16_task5.kt
+++ src/main/kotlin/lesson_16/lesson16_task5.kt
@@ -2,7 +2,7 @@ package org.example.lesson_16
 
 fun main() {
     val player = Player("Боец")
-    while (player.getHP() > 0) {
+    while (player.isPlayerAlive()) {
         println("\nНовый раунд:")
         player.showStats()
         player.dealDamage()
@@ -16,37 +16,38 @@ class Player(
 ) {
     private var healthPoints = 100
     private val hitForce = 15
+    private var isAlive = true
 
     fun showStats() {
         println("У игрока [$name]: $healthPoints очков здоровья")
     }
 
     fun dealDamage() {
-        if (healthPoints > 0)
+        if (isAlive)
             println("[$name] наносит противнику $hitForce очков урона")
-        else
-            return
     }
 
     fun takeDamage(damage: Int) {
         println("Противник наносит игроку $damage очков урона")
         healthPoints -= damage
-        if (healthPoints <= 0) {
-            println("$name убит")
-            healthPoints = 0
-            return
-        }
+        checkingIsDead()
     }
 
     fun heal(healPoints: Int) {
-        if (healthPoints > 0) {
+        if (isAlive) {
             healthPoints += healPoints
             println("[$name] вылечился на $healPoints очков здоровья")
-        } else
-            return
+        }
     }
 
-    fun getHP(): Int {
-        return healthPoints
+    private fun checkingIsDead(){
+        if (healthPoints <= 0) {
+            isAlive = false
+            println("$name убит")
+        }
+    }
+
+    fun isPlayerAlive(): Boolean {
+        return isAlive
     }
 }
